fa1775117edd1847fc43c21913b92ec58e461ade
Ian Freitas <ianfreitas1@poli.ufrj.br>
2021-08-22 17:51:41 -0300

    :passport_control: Adds protect middleware to requests routes

--- routes/requests.js
+++ routes/requests.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const { createRequest, getAllRequests, getRequest, updateRequest, deleteRequest} = require('../controllers/requests');
+const {
+  createRequest,
+  getAllRequests,
+  getRequest,
+  updateRequest,
+  deleteRequest,
+} = require('../controllers/requests');
+const { protect } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get('/:id', getRequest);
 router.get('', getAllRequests);
-router.post('', createRequest);
-router.put('/:id', updateRequest);
-router.delete('/:id', deleteRequest);
+router.post('', protect, createRequest);
+router.put('/:id', protect, updateRequest);
+router.delete('/:id', protect, deleteRequest);
 
 module.exports = router;
