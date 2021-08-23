5823a25fa664f2f1e6eac8b743686d8569220bbf
Jéssica Martins <jessica.moliveira@g.globo>
2021-08-23 00:52:07 -0300

    :sparkles: create login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
         </Switch>
       </BrowserRouter>
     </div>
