e1aca03fb096a38736f513e1d86c756ef6b9e0fe
Jéssica Martins <jessica.moliveira@g.globo>
2021-08-23 01:18:32 -0300

    :lipstick: create initial sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={RegisterPage} />
         </Switch>
       </BrowserRouter>
     </div>
