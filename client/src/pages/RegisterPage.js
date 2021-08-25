8841b58cc6547e27be63e3aab1c16035d1bc360f
Ian Freitas <ianfreitas1@poli.ufrj.br>
2021-08-25 20:16:25 -0300

    :sparkles: Adds sign up logic

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -8,25 +8,63 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { registerUser } from '../api/authApi';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  error: {
+    color: 'red',
+  },
+}));
 
 const RegisterPage = () => {
-  const useStyles = makeStyles(theme => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    form: {
-      width: '100%',
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
-
   const classes = useStyles();
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [major, setMajor] = useState('');
+  const [age, setAge] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) history.push('/dashboard');
+  }, []);
+
+  const handleSignUp = async e => {
+    e.preventDefault();
+    const body = {
+      name,
+      email,
+      password,
+      major,
+      age,
+    };
+
+    try {
+      const { user, token } = await registerUser(body);
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('token', JSON.stringify(token));
+      setError(null);
+      history.push('/dashboard');
+    } catch (error) {
+      if (!error) setError('Failed to sign up. Please try again in a moment.');
+      else setError(error.message);
+    }
+  };
 
   return (
     <>
@@ -37,23 +75,38 @@ const RegisterPage = () => {
           </Typography>
           <form className={classes.form} noValidate>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
                 <TextField
                   variant="outlined"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="name"
+                  label="Name"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
                   autoFocus
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
                 <TextField
                   variant="outlined"
                   required
                   fullWidth
-                  id="lastName"
-                  label="Last Name"
+                  id="age"
+                  label="Age"
+                  value={age}
+                  onChange={e => setAge(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="major"
+                  label="Major"
+                  value={major}
+                  onChange={e => setMajor(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -63,6 +116,8 @@ const RegisterPage = () => {
                   fullWidth
                   id="email"
                   label="Email Address"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -73,15 +128,21 @@ const RegisterPage = () => {
                   label="Password"
                   type="password"
                   id="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
                 />
               </Grid>
             </Grid>
+            {error && (
+              <Typography className={classes.error}>{error}</Typography>
+            )}
             <Button
               type="submit"
               fullWidth
               variant="contained"
               color="primary"
               className={classes.submit}
+              onClick={handleSignUp}
             >
               Sign Up
             </Button>
