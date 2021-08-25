1f017639bb0dadf9584a0dcd9454839bc846b0bc
Ian Freitas <ianfreitas1@poli.ufrj.br>
2021-08-25 19:59:41 -0300

    :sparkles: Adds header login and logout functionalities

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setLoggedIn(true);
+    }
+  }, [setLoggedIn]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    history.push('/login');
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu"></IconButton>
-        <Typography variant="h6">Tutora</Typography>
-        <Button color="inherit" onClick={() => console.log('login')}>
-          Login
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Tutora
+          </Typography>
+          {!loggedIn && (
+            <Button component={Link} to={'/login'} color="inherit">
+              Login
+            </Button>
+          )}
+          {loggedIn && (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
