d19a4dd4246f832b6a7e22201e90752504f2ec5c
Gustavo Machado <gustavo.machado@cern.ch>
2021-10-11 18:05:13 -0300

    :truck: Add new route for page with requests that the User is enrolled in

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import DashboardPage from './pages/DashboardPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import RequestListPage from './pages/RequestListPage';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/signup" component={RegisterPage} />
           <PrivateRoute path="/dashboard" component={DashboardPage} />
+          <PrivateRoute path="/myRequests" component={RequestListPage} />
         </Switch>
       </BrowserRouter>
     </div>
